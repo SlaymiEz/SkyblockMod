47fcf285efae3026a7cb65eac6d5aec4e7c54ad2
SlaymiEz <tt208741@gmail.com>
2024-02-22 12:48:08 +0100

    Added a blacklist system and fixed the typo

--- src/main/java/fr/slaymi/ChestCommand.java
+++ src/main/java/fr/slaymi/ChestCommand.java
@@ -5,10 +5,14 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.ChatComponentText;
 
-public class ChestCommand extends CommandBase {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ChestCommand extends CommandBase {
+    public static List<BlockPos> blPos = new ArrayList<>();
     @Override
     public String getCommandName() {
         return "chestConfig";
@@ -25,10 +29,10 @@ public class ChestCommand extends CommandBase {
             if (args[0].equals("set")){
                 if (args[1].equals("on")){
                     ChestTracer.actived = true;
-                    chatLog("Succesfully turned ON the ESP");
+                    chatLog("Successfully turned ON the ESP");
                 } else if (args[1].equals("off")) {
                     ChestTracer.actived = false;
-                    chatLog("Succesfully turned OFF the ESP");
+                    chatLog("Successfully turned OFF the ESP");
                 } else {
                     CommandException exception = new CommandException("get rekt idiot");
                     throw exception;
@@ -36,8 +40,23 @@ public class ChestCommand extends CommandBase {
             }
             if (args[0].equals("width")) {
                 float f = Float.valueOf(args[1]);
-                chatLog("Succesfully set the width to " + f);
+                chatLog("Successfully set the width to " + f);
                 ChestTracer.width = f;
+            }
+            if (args[0].equals("bl")){
+                if (args[1].equals("add")){
+                    blPos.addAll(ChestTracer.chestPositions);
+                    chatLog("Successfully blacklisted all annoying chests");
+                } else if (args[1].equals("clear")){
+                    blPos.clear();
+                    chatLog("Successfully cleared the blacklist");
+                } else {
+                    CommandException exception = new CommandException("get rekt idiot");
+                    throw exception;
+                }
+            } else {
+                CommandException exception = new CommandException("get rekt idiot");
+                throw exception;
             }
         } else {
             CommandException exception = new CommandException("get rekt idiot");
--- src/main/java/fr/slaymi/ChestTracer.java
+++ src/main/java/fr/slaymi/ChestTracer.java
@@ -1,34 +1,33 @@
 package fr.slaymi;
 
 
+
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.RenderGlobal;
-import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.AxisAlignedBB;
-import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.block.BlockChest;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.BlockPos;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-import org.lwjgl.Sys;
 import org.lwjgl.opengl.GL11;
-import scala.tools.nsc.doc.model.Def;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ChestTracer {
+    public static List<BlockPos> chestPositions = new ArrayList<>();
     public static final int SEARCH_RADIUS = 15;
     public static boolean actived = false;
     public static float width = (float) 8.0;
-    private Minecraft mc = Minecraft.getMinecraft();
+    private static Minecraft mc = Minecraft.getMinecraft();
     private ChestGUI chestText = new ChestGUI();
 
     public static int nearbyChestsPublic = 0;
@@ -36,7 +35,7 @@ public class ChestTracer {
     private static int nearbyChests = 0;
     @SubscribeEvent
     public void onRenderWorldLast(RenderWorldLastEvent event){
-        renderTracers(event.partialTicks);
+        renderESP(event.partialTicks);
     }
     @SubscribeEvent
     public void onRenderGameOverlay(RenderGameOverlayEvent.Text event){
@@ -50,17 +49,21 @@ public class ChestTracer {
         chestText.updateScreen();
     }
 
-    public static void renderTracers(float partialTick){
+    public static void renderESP(float partialTick){
         EntityPlayer player = Minecraft.getMinecraft().thePlayer;
         nearbyChests = 0;
+        chestPositions.clear();
         for (int x = (int) player.posX - SEARCH_RADIUS; x <= (int) player.posX + SEARCH_RADIUS; x++){
             for(int y = (int) player.posY - SEARCH_RADIUS; y <= (int) player.posY + SEARCH_RADIUS; y++) {
                 for (int z = (int) player.posZ - SEARCH_RADIUS; z <= (int) player.posZ + SEARCH_RADIUS; z++) {
                     BlockPos pos = new BlockPos(x, y, z);
                     if (Minecraft.getMinecraft().theWorld.getBlockState(pos).getBlock() instanceof BlockChest) {
                         if (actived == true) {
-                            blockESPBox(pos);
-                            nearbyChests += 1;
+                            if (!chestPositions.contains(pos)) chestPositions.add(pos);
+                            if (!ChestCommand.blPos.contains(pos)) {
+                                blockESPBox(pos);
+                                nearbyChests += 1;
+                            }
                         }
                     }
                 }
@@ -69,17 +72,20 @@ public class ChestTracer {
         nearbyChestsPublic = nearbyChests;
     }
     private static void renderTracer(EntityPlayer player, double chestX, double chestY, double chestZ){
-        WorldRenderer worldRenderer = Tessellator.getInstance().getWorldRenderer();
-        GlStateManager.pushAttrib();
-        GlStateManager.pushMatrix();
+        GL11.glPushMatrix();
+        GL11.glTranslated(mc.getRenderManager().viewerPosX, mc.getRenderManager().viewerPosY, mc.getRenderManager().viewerPosZ);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glLineWidth(width);
 
-        worldRenderer.begin(3, DefaultVertexFormats.POSITION_COLOR);
-        worldRenderer.pos(player.posX, player.posY + player.getEyeHeight(), player.posZ).color(1.0F, 0.0F, 0.0F, 1.0F).endVertex();
-        worldRenderer.pos(chestX, chestY, chestZ).color(1.0F, 0.0F, 0.0F, 1.0F).endVertex();
+        Tessellator tessellator = Tessellator.getInstance();
+        WorldRenderer worldRenderer = tessellator.getWorldRenderer();
 
-        Tessellator.getInstance().draw();
-        GlStateManager.popMatrix();
-        GlStateManager.pushAttrib();
+        worldRenderer.begin(GL11.GL_LINE_STRIP, DefaultVertexFormats.POSITION_COLOR);
+
+        
     }
     public static void blockESPBox(BlockPos blockPos) {
 
